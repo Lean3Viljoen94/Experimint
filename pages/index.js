fcbe5b5e4568d2748ba89f2026b24b49f063452a
Leandri Nieuwoudt <leandri@sweepsouth.com>
2020-02-18 09:51:48 +0200

    adding some comments

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,11 @@
         <div>
             <h1>Welcome to the Experimint!</h1>
                 <p>Yes the spelling error is intentional</p>
+            <div>
+                <h1>
+                    Haha, only saw this now.. I assumed it is!
+                </h1>
+            </div>
         </div>
 )
 
